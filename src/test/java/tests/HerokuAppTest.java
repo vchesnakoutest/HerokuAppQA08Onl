4da9dccae81a08065cf179b2ee75d5735e5b2d0f
Valiantsin_Chesnakou <Valiantsin.Chesnakou@gmail.com>
2021-10-25 09:01:18 +0300

    add files

--- src/test/java/tests/HerokuAppTest.java
+++ src/test/java/tests/HerokuAppTest.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class HerokuAppTest {
@@ -27,6 +29,42 @@ public class HerokuAppTest {
         inputField.sendKeys("1111");
         String textFromField = inputField.getAttribute("value");
         Assert.assertEquals(textFromField, "1111");
+        driver.quit();
+    }
+
+    @Test
+    public void herokuAppDropDownTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setHeadless(false);
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("http://the-internet.herokuapp.com/dropdown");
+        Select select = new Select(driver.findElement(By.id("dropdown")));
+        select.selectByVisibleText("Option 2");
+        String selectedOption = select.getFirstSelectedOption().getText();
+        Assert.assertEquals(selectedOption, "Option 2");
+        driver.quit();
+    }
+
+    @Test
+    public void herokuAddRemoveElementsTest (){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--incognito");
+        chromeOptions.addArguments("--disable-popup-blocking");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+        WebElement addRemoveElements = driver.findElement(By.xpath("//*[@onclick='addElement()']"));
+        addRemoveElements.click();
+        addRemoveElements.click();
+        driver.findElement(By.xpath("//*[@onclick='deleteElement()']")).click();
+        int count = driver.findElements(By.xpath("//*[@onclick='deleteElement()']")).size();
+        System.out.println(count);
         driver.quit();
     }
 }
